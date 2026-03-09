24ceb3dcdc8e97492688046ccddb3189af501ac1
KevinMahecha12 <kevinmahecha@outlook.com>
2026-03-09 15:29:07 -0600

    chore: setup routes and vercel config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';// Ajusta la ruta a tu archivo
+import { PokemonListComponent } from './features/pokemon/components/pokemon-list/pokemon-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PokemonListComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
